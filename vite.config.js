e30d3868e052829e730629b8d8897420bbada644
Keisuke Nemoto <n@on-lab.jp>
2021-05-12 16:32:17 +0900

    vite server proxy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/socket.io/': 'http://localhost:3333/socket.io/',
+    },
+  }
 })
